69066a2841cb28c8d13b29460ab6eba87fbb17e7
gokulkatragadda <gokulsaikatragadda@gmail.com>
2020-02-23 23:25:39 -0500

    Remove Main Storyboard

---
+++ GithubSampleApp/GithubSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSampleApp
+//
+//  Created by Gokul Sai Katragadda on 2/23/20.
+//  Copyright © 2020 AppFactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
